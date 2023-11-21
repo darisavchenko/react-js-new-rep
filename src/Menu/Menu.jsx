a0f31e53c72ceb381f4fc50d281391ac968b5fc8
darisavchenko <daria.savchenko@stud.h-da.de>
2023-11-21 09:26:43 +0100

    menu added

---
+++ src/Menu/Menu.jsx
@@ -0,0 +1,17 @@
+import "./Menu.css";
+
+export function Menu() {
+  return (
+    <div className="menu-wrap">
+      <a href="" className="menu-link">
+        Home
+      </a>
+      <a href="" className="menu-link">
+        Tasks
+      </a>
+      <a href="" className="menu-link">
+        Other
+      </a>
+    </div>
+  );
+}
